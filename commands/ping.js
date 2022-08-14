08aafd72055ebffc73ba49048123cfa6133cf584
Suurow <benjamin.breyne@hotmail.com>
2022-08-14 22:59:31 +0200

    babyy-bot

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDescription('Respond with Pong'),
+    async execute(interaction) {
+        await interaction.reply('pong!');
+    }
+}
---
+++ commands/suggest.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, PermissionFlagsBits, CommandInteraction, EmbedBuilder, WebhookClient} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('suggest')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+    .setDescription('Faites une suggestions.')
+    .addStringOption(option =>
+        option.setName('texte')
+                .setDescription('écrit ta suggestion ici')
+                .setRequired(true)),
+    /**
+     * 
+     * @param {CommandInteraction} interaction
+     */
+    async execute(interaction, msg) {
+        const suggestChannel = interaction.guild.channels.cache.get('1008445644727390248')
+        const suggestText = interaction.options.getString('texte');
+
+        const Response = new EmbedBuilder()
+        .setColor("DarkGold")
+        .setThumbnail(interaction.user.avatarURL({dynamic: true}))
+        .setTitle(`Suggestion de ${interaction.user.tag}`)
+        .setDescription(`${suggestText}`)
+        .setTimestamp();
+        
+        const message = await suggestChannel.send({embeds: [Response], fetchReply: true})
+        message.react("✔️")
+        message.react("❌")
+        interaction.reply({ content: `Ta suggestion a été publié ici -> **${suggestChannel}**`, ephemeral: true});
+    }
+}
