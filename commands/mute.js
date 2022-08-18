e259220e09cc5220363f4b592fd4ae015b6aa958
Suurow <benjamin.breyne@hotmail.com>
2022-08-18 21:02:17 +0200

    mute fix bug Logs

--- commands/mute.js
+++ commands/mute.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDMPermission(false)
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
     .setDescription("Rend muet l'utilisateur mentionné indéfiniment.")
-    .addUserOption(option => option.setName('member').setDescription('Sélectionne le membre à rendre muet')),
+    .addUserOption(option => option.setName('member').setDescription('Sélectionne le membre à rendre muet').setRequired(true)),
 
         /**
          * 
@@ -18,18 +18,21 @@ module.exports = {
         const Logs = new EmbedBuilder().setColor('Aqua');
 
         const member = interaction.options.getMember('member');
+
         if(!interaction.guild.roles.cache.has('1009791332241379328')){
             interaction.reply({embeds: [new EmbedBuilder().setColor('Red').setDescription(`🔴 Le rôle **Muet** n'existe pas`)], ephemeral: true})
         }
+
         if(member.roles.cache.has('1009791332241379328')){
-            interaction.reply({embeds: [new EmbedBuilder().setColor('Orange').setDescription(`🟠 ${member} est déjà muet`)], ephemeral: true})
+            interaction.reply({embeds: [new EmbedBuilder().setColor('Orange').setDescription(`🟠 ${member} est déjà muet`)], ephemeral: true, })
         } else {
             member.roles.add('1009791332241379328')
             await interaction.reply({embeds: [new EmbedBuilder().setColor('Green').setDescription(`🟢 ${member} est désormais muet`)]})
             // Logs
             Logs.setDescription(`${interaction.user} a rendu muet ${member}. \n Commandes utilisé dans ${interaction.channel}.`)
+            Loger.send({embeds: [Logs]})
         }
         // Sending Logs
-        Loger.send({embeds: [Logs]})
+        
     }
 }
