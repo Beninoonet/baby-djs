7a9a35dae1d816bf17b267bbb60eab3099b43a66
Suurow <benjamin.breyne@hotmail.com>
2022-08-20 22:00:04 +0200

    purge fix

--- commands/purge.js
+++ commands/purge.js
@@ -24,19 +24,16 @@ module.exports = {
             .setColor('DarkRed');
         
         const Logs = new EmbedBuilder()
-            .setColor('DarkOrange')
-            .setTitle(`${interaction.user.tag} à utilisé ${interaction.commandName}`);
+            .setColor('DarkOrange');
         
         if(message > 100){
             Response.setDescription('**Tu ne peux pas supprimé plus de 100 messages**');
             interaction.reply({embeds: [Response], ephemeral: true});
-            return;
         };
 
         if(message < 0){
             Response.setDescription('**Tu dois supprimé au minimum 1 message**');
             interaction.reply({embeds: [Response], ephemeral: true});
-            return;
         };
 
         if(target){
@@ -45,22 +42,25 @@ module.exports = {
             Response.setDescription(`**Le(s) message(s) de ${target} ont été supprimé**`);
             Logs.setDescription(`${interaction.user} a supprimé les message de ${target} dans le salon ${interaction.channel}.`)
 
-            interaction.reply({embeds: [Response], fetchReply: true})
+            interaction.reply({embeds: [Response]})
+            
             setTimeout(() => {
                 interaction.deleteReply()
             }, 5000);
+            Loger.send({embeds: [Logs]})
         }
         else{
             interaction.channel.bulkDelete(message)
             Response.setDescription(`**${message} message(s) supprimé**`);
             Logs.setDescription(`${interaction.user} a supprimé ${message} message(s) dans le salon ${interaction.channel}.`)
 
+            interaction.reply({embeds: [Response]})
             
-            interaction.reply({embeds: [Response], fetchReply: true})
             setTimeout(() => {
                 interaction.deleteReply()
             }, 5000);
-        }
-        Loger.send({embeds: [Logs]})
+            Loger.send({embeds: [Logs]})
+        };
+        
     }
 }
