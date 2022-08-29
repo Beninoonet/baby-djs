5063098ebc35e40852533828d23cda36743e4ad1
Suurow <benjamin.breyne@hotmail.com>
2022-08-29 02:12:07 +0200

    PING BOT

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, Client, CommandInteraction, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,28 +6,7 @@ module.exports = {
     .setDMPermission(false)
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .setDescription('Respond with Pong'),
-    /**
-     * 
-     * @param {CommandInteraction} interaction 
-     * @param {Client} client 
-     */
-    async execute(interaction, client) {
-
-        const tryPong = interaction.reply({content: "En attente de la réponse.", fetchReply: true});
-
-        const Response = new EmbedBuilder()
-        .setTitle('Pong! 🟢')
-        .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-        .addFields(
-            { name: "Latence API.", value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true},
-            { name: "Latence Bot.", value: `\`\`\`${(await tryPong).createdTimestamp - interaction.createdTimestamp}ms\`\`\``, inline: true}
-        )
-        .setTimestamp()
-        .setFooter({
-            text: interaction.user.username,
-            iconURL: interaction.user.displayAvatarURL(),
-        });
-
-        interaction.editReply({embeds: [embeds]});
+    async execute(interaction) {
+        await interaction.reply(`**Latence API** \`\`\`${interaction.client.ws.ping}ms\`\`\``);
     }
 }
